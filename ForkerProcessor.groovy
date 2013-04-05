bce7b715517fcde3154817e47a859de79749eedc
wushexu <wushexu@gmail.com>
2013-04-05 22:39:35 +0800

    Create ForkerProcessor.groovy

---
+++ ForkerProcessor.groovy
@@ -0,0 +1,39 @@
+package xy.pars
+
+import xy.pars.message.*
+
+
+class ForkerProcessor extends Processor {
+    
+    protected List<Processor> forkSuccessors
+  
+	void setForkSuccessors(List<Processor> forkSuccessors){
+		this.forkSuccessors=forkSuccessors
+		forkSuccessors.each{
+			it.previous << this
+		}
+	}
+    
+    def beforeInit(){
+	}
+	
+	Result process(Task task){
+		
+		forkSuccessors?.each{ successor ->
+			def successorQueue=successor.tasksQueue
+			while(successorQueue.length() >= Processor.MAX_QUEUE_LENGTH){
+				Thread.yield()
+			}
+			successorQueue << task
+		}
+		
+		return null
+	}
+	
+	def onCompleted(){
+		def completedMessage=new Completed(sender:this)
+		forkSuccessors?.each { it << completedMessage }
+		super.onCompleted()
+	}
+	
+}
